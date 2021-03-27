fabc99e033f0ce73713ca175e10032bca724705e
woniuasd <kk18810532057@gmail.com>
2021-03-27 13:24:55 +0800

    删除无关代码

--- mini-caffe.cmake
+++ mini-caffe.cmake
@@ -18,41 +18,41 @@ if(CMAKE_COMPILER_IS_GNUCXX OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang"))
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 endif()
 
-# include and library
-if(MSVC)
-  include_directories(${CMAKE_CURRENT_LIST_DIR}/3rdparty/include
-                      ${CMAKE_CURRENT_LIST_DIR}/3rdparty/include/openblas
-                      ${CMAKE_CURRENT_LIST_DIR}/3rdparty/include/google
-                      ${CMAKE_CURRENT_LIST_DIR}/include)
-  link_directories(${CMAKE_CURRENT_LIST_DIR}/3rdparty/lib)
-  list(APPEND Caffe_LINKER_LIBS debug libprotobufd optimized libprotobuf
-                                libopenblas)
-elseif(ANDROID)
-  # TODO https://github.com/android-ndk/ndk/issues/105
-  set(CMAKE_CXX_STANDARD_LIBRARIES "${CMAKE_CXX_STANDARD_LIBRARIES} -nodefaultlibs -lgcc -lc -lm -ldl")
-  if(ANDROID_EXTRA_LIBRARY_PATH)
-    include_directories(${CMAKE_CURRENT_LIST_DIR}/include
-                        ${ANDROID_EXTRA_LIBRARY_PATH}/include)
-    link_directories(${ANDROID_EXTRA_LIBRARY_PATH}/lib)
-    list(APPEND Caffe_LINKER_LIBS openblas protobuf log)
-  else(ANDROID_EXTRA_LIBRARY_PATH)
-    message(FATAL_ERROR "ANDROID_EXTRA_LIBRARY_PATH must be set.")
-  endif(ANDROID_EXTRA_LIBRARY_PATH)
-else(MSVC)
-  if(APPLE)
-    include_directories(/usr/local/opt/openblas/include)
-    link_directories(/usr/local/opt/openblas/lib)
-  endif(APPLE)
-  include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
-  list(APPEND Caffe_LINKER_LIBS protobuf)
-  if(BLAS STREQUAL "openblas")
-    list(APPEND Caffe_LINKER_LIBS openblas)
-    message(STATUS "Use OpenBLAS for blas library")
-  else()
-    list(APPEND Caffe_LINKER_LIBS blas)
-    message(STATUS "Use BLAS for blas library")
-  endif()
-endif(MSVC)
+# # include and library
+# if(MSVC)
+#   include_directories(${CMAKE_CURRENT_LIST_DIR}/3rdparty/include
+#                       ${CMAKE_CURRENT_LIST_DIR}/3rdparty/include/openblas
+#                       ${CMAKE_CURRENT_LIST_DIR}/3rdparty/include/google
+#                       ${CMAKE_CURRENT_LIST_DIR}/include)
+#   link_directories(${CMAKE_CURRENT_LIST_DIR}/3rdparty/lib)
+#   list(APPEND Caffe_LINKER_LIBS debug libprotobufd optimized libprotobuf
+#                                 libopenblas)
+# elseif(ANDROID)
+#   # TODO https://github.com/android-ndk/ndk/issues/105
+#   set(CMAKE_CXX_STANDARD_LIBRARIES "${CMAKE_CXX_STANDARD_LIBRARIES} -nodefaultlibs -lgcc -lc -lm -ldl")
+#   if(ANDROID_EXTRA_LIBRARY_PATH)
+#     include_directories(${CMAKE_CURRENT_LIST_DIR}/include
+#                         ${ANDROID_EXTRA_LIBRARY_PATH}/include)
+#     link_directories(${ANDROID_EXTRA_LIBRARY_PATH}/lib)
+#     list(APPEND Caffe_LINKER_LIBS openblas protobuf log)
+#   else(ANDROID_EXTRA_LIBRARY_PATH)
+#     message(FATAL_ERROR "ANDROID_EXTRA_LIBRARY_PATH must be set.")
+#   endif(ANDROID_EXTRA_LIBRARY_PATH)
+# else(MSVC)
+#   if(APPLE)
+#     include_directories(/usr/local/opt/openblas/include)
+#     link_directories(/usr/local/opt/openblas/lib)
+#   endif(APPLE)
+#   include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
+#   list(APPEND Caffe_LINKER_LIBS protobuf)
+#   if(BLAS STREQUAL "openblas")
+#     list(APPEND Caffe_LINKER_LIBS openblas)
+#     message(STATUS "Use OpenBLAS for blas library")
+#   else()
+#     list(APPEND Caffe_LINKER_LIBS blas)
+#     message(STATUS "Use BLAS for blas library")
+#   endif()
+# endif(MSVC)
 
 # source file structure
 file(GLOB CAFFE_INCLUDE ${CMAKE_CURRENT_LIST_DIR}/include/caffe/*.h
